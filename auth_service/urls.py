f1cb4e7e6e81683b3bf0a93f0d8060bf68e65654
Ananthu <ananthup303@gmail.com>
2025-08-31 09:46:31 +0100

    Swagger docs

--- auth_service/urls.py
+++ auth_service/urls.py
@@ -25,6 +25,6 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('users.urls')),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),  # JSON OpenAPI schema
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),  # Swagger UI
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
